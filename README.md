1e851f74bec203c1d6dbec4aebea392f87d97e67
Eddie Kao <eddie@5xcampus.com>
2023-04-24 19:28:54 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ezPay
+
+- 發票
+
+## 參考資料
+
+- [電子發票技術串接手冊](https://inv.ezpay.com.tw/dw_files/info_api/EZP_INVI_1_2_1.pdf)
+
+by eddie@5xcampus.com
---
+++ lib/ezpay.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ezpay/version"
+
+module Ezpay
+  class Error < StandardError; end
+  # Your code goes here...
+end
