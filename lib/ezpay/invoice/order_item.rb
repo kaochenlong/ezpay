2e9d0363e375469f77fdb7107b878f39e77bb09c
Eddie Kao <eddie@5xcampus.com>
2023-04-28 13:49:21 +0800

    fix total_amount in different tax type and tax rate

--- lib/ezpay/invoice/order_item.rb
+++ lib/ezpay/invoice/order_item.rb
@@ -9,13 +9,23 @@ module Ezpay
       attr_reader :name, :quantity, :price, :unit, :tax
 
       def initialize(
-        name:,
+        name: nil,
+        price: nil,
         quantity: 1,
-        price:,
         unit: ENV["DEFAULT_UNIT"],
         tax_type: :taxable,
         tax_rate: ENV["DEFAULT_TAX_RATE"].to_i
       )
+        if name.nil?
+          raise Ezpay::Invoice::Error::OrderItemFieldMissingError,
+                "item name is required"
+        end
+
+        if price.nil?
+          raise Ezpay::Invoice::Error::OrderItemFieldMissingError,
+                "item price is required"
+        end
+
         @name = name
         @quantity = quantity
         @price = price
@@ -23,8 +33,15 @@ module Ezpay
         @tax = Tax.new(type: tax_type, rate: tax_rate)
       end
 
+      # 法規名稱：加值型及非加值型營業稅法
+      # 第 14 條 營業人銷售貨物或勞務，除本章第二節另有規定外，均應就銷售額，分別按第七條或第十條規定計算其銷項稅額
+      # 尾數不滿通用貨幣一元者，按四捨五入計算。
       def total_amount
-        quantity * price
+        if taxable?
+          (quantity * price * ((100 + tax_rate) / 100.0)).round
+        else
+          quantity * price
+        end
       end
 
       def tax_rate
@@ -32,18 +49,14 @@ module Ezpay
       end
 
       def tax_rate=(new_tax_rate = 0)
-        @tax.rate = new_tax_rate if new_tax_rate > 0
+        @tax.rate = new_tax_rate if new_tax_rate.positive?
       end
 
       # 設定稅別
       def set_tax(type:, rate: 0)
         @tax.type = type
 
-        if %i[tax_exemption tax_zero].include?(type)
-          @tax.rate = 0
-        else
-          @tax.rate = rate
-        end
+        @tax.rate = (%i[tax_exemption tax_zero].include?(type) ? 0 : rate)
       end
 
       def_delegators :@tax, :taxable?, :tax_exemption?, :tax_zero?
--- spec/invoice/orders/order_item_spec.rb
+++ spec/invoice/orders/order_item_spec.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe Ezpay::Invoice::OrderItem do
+  let(:item) { Ezpay::Invoice::OrderItem.new(name: "Hello World", price: 100) }
+
   context "建立訂單物件" do
-    it "可以建立訂單物件，並計算該項金額小計" do
+    it "可以建立訂單物件" do
       item =
         Ezpay::Invoice::OrderItem.new(
           name: "為你自己學 Git",
@@ -11,15 +13,20 @@ RSpec.describe Ezpay::Invoice::OrderItem do
           unit: "個"
         )
 
-      expect(item.total_amount).to be 300
       expect(item.unit).to eq "個"
+      expect(item.price).to be 150
+      expect(item.quantity).to be 2
+    end
+
+    it "沒填寫必填品項及價錢會拋出錯誤" do
+      expect { Ezpay::Invoice::OrderItem.new }.to raise_error(
+        Ezpay::Invoice::Error::OrderItemFieldMissingError
+      )
     end
   end
 
   context "數量及單位" do
     it "如果沒填寫數量，預設數量 1，單位「件」" do
-      item = Ezpay::Invoice::OrderItem.new(name: "Hello World", price: 100)
-
       expect(item.quantity).to be 1
       expect(item.unit).to eq "件"
     end
@@ -27,15 +34,11 @@ RSpec.describe Ezpay::Invoice::OrderItem do
 
   context "稅別" do
     it "如果沒填寫稅別，預設為「應稅」，稅率為「預設稅率」" do
-      item = Ezpay::Invoice::OrderItem.new(name: "Hello World", price: 100)
-
       expect(item).to be_taxable
       expect(item.tax_rate).to be 5
     end
 
     it "可改變特定 item 的稅率" do
-      item = Ezpay::Invoice::OrderItem.new(name: "Hello World", price: 100)
-
       item.tax_rate = 8
 
       expect(item).to be_taxable
@@ -43,7 +46,6 @@ RSpec.describe Ezpay::Invoice::OrderItem do
     end
 
     it "可變更稅別" do
-      item = Ezpay::Invoice::OrderItem.new(name: "Hello World", price: 100)
       expect(item).to be_taxable
 
       item.set_tax(type: :tax_exemption)
@@ -57,6 +59,29 @@ RSpec.describe Ezpay::Invoice::OrderItem do
       item.set_tax(type: :taxable, rate: 8)
       expect(item).to be_taxable
       expect(item.tax_rate).to be 8
+    end
+
+    context "可依據不同稅別計算小計金額" do
+      let(:item) do
+        Ezpay::Invoice::OrderItem.new(
+          name: "Hello World",
+          price: 120,
+          quantity: 2
+        )
+      end
+
+      it "應稅" do
+        expect(item.total_amount).to be 240 *
+             (100 + ENV["DEFAULT_TAX_RATE"].to_i) / 100
+      end
+
+      it "免稅及零稅率" do
+        item.set_tax(type: :tax_exemption)
+        expect(item.total_amount).to be 240
+
+        item.set_tax(type: :tax_zero)
+        expect(item.total_amount).to be 240
+      end
     end
   end
 end
